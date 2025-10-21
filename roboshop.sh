cc7e4018d33f6138b7d1605d2ef8631a23870324
Sandeep Chukkapalli <sandeep.chukkapalli@gmail.com>
2025-10-21 19:50:39 +0530

    Enhance

--- roboshop.sh
+++ roboshop.sh
@@ -9,9 +9,9 @@ do
     Intsance_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-0eb4931c1eaee29a2 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
 
     if [ $instance != "frontend" ]; then
-        IP=$(aws ec2 describe-instances --instance-ids i-0d258a5c894512d9a --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+        IP=$(aws ec2 describe-instances --instance-ids $Intsance_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
     else
-        IP=$(aws ec2 describe-instances --instance-ids i-0d258a5c894512d9a --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+        IP=$(aws ec2 describe-instances --instance-ids $Intsance_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
     fi
 
     echo "$instance: $IP"
